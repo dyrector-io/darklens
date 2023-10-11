059995b21531912ef19219c9cfcc57e601e43ba5
Bence Nagygyörgy <robot9706@gmail.com>
2023-10-11 14:08:57 +0200

    fix: node edit & labels

--- web/frontend/src/components/nodes/edit-node-card.tsx
+++ web/frontend/src/components/nodes/edit-node-card.tsx
@@ -58,6 +58,7 @@ const EditNodeCard = (props: EditNodeCardProps) => {
             ...values,
             id: node.id,
             status: node.status,
+            hasToken: node.hasToken,
           } as NodeDetails
         }
 
